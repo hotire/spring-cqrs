afc95c90d7cbafe7f6ec5a8c3532d93f0fdd0b79
hotire <gngh0101@gmail.com>
2021-09-29 02:06:14 +0900

    Simplify

--- src/main/kotlin/com/github/hotire/spring/cqrs/Application.kt
+++ src/main/kotlin/com/github/hotire/spring/cqrs/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringCqrsApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringCqrsApplication>(*args)
+    runApplication<Application>(*args)
 }
