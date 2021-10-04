24ab9cffeb5f0a2e431386d3f037996886f7a02b
hotire <gngh0101@gmail.com>
2021-10-04 19:55:53 +0900

    Implement create / update user

--- src/main/kotlin/com/github/hotire/spring/cqrs/ex/UserRepository.kt
+++ src/main/kotlin/com/github/hotire/spring/cqrs/ex/UserRepository.kt
@@ -2,6 +2,17 @@ package com.github.hotire.spring.cqrs.ex
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository
+interface UserRepository {
+    fun createUser(user: User): User
+    fun updateUser(user: User): User
+}
 
-interface UserEntityRepository : JpaRepository<UserEntity, Long>, UserRepository
+interface UserEntityRepository : JpaRepository<UserEntity, Long>, UserRepository {
+    override fun createUser(user: User): User {
+        return user
+    }
+
+    override fun updateUser(user: User): User {
+        return user
+    }
+}
