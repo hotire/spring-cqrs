56a60f04b4961f607c59795614e16a8cc1da2adf
hotire <gngh0101@gmail.com>
2021-10-04 20:04:00 +0900

    Implement UserMapper

---
+++ src/main/kotlin/com/github/hotire/spring/cqrs/ex/UserMapper.kt
@@ -0,0 +1,15 @@
+package com.github.hotire.spring.cqrs.ex
+
+import org.mapstruct.Mapper
+import org.mapstruct.factory.Mappers
+
+@Mapper
+interface UserMapper {
+
+    companion object {
+        val INSTNACE = Mappers.getMapper(UserMapper::class.java)
+    }
+
+    fun toDomain(userEntity: UserEntity): User
+    fun toJpaEntity(user: User): UserEntity
+}
